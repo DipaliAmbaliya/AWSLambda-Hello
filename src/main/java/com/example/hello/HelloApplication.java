d6d2eded485303fd5a4e9a412aa7a51f8fa20d20
dipali.ambaliya <simform.dipali.ambaliya@carsaver.com>
2024-04-26 18:38:21 +0530

    Hello World

---
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -0,0 +1,8 @@
+package com.example.hello;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HelloApplication {
+}
